d768961864467837a0308fefd9f7b6e669b9aead
salim <s534027@nwmissouri.edu>
2021-05-10 14:26:20 -0500

    Created the page header for the app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Hello world
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
+  },
+  //Create placeholders to store the data from the API
+  data(){
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/hourglass.gif')
+    }
+  },
+  methods: {
+    //Create method to get the covid data I need from the API
+    async fetchCovidData(){
+      const res = await fetch('https://api.covid19api.com/summary');
+      const data = await res.json();
+      return data
+    }
+  },
+
+  //Runs as soon as the site is loaded
+  async created(){
+    const data = await this.fetchCovidData()
+  
+    this.dataDate = data.date
+    this.stats = data.global
+    this.countries = data.countries
+    this.loadingImage = false
   }
 }
 </script>
