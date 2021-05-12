b9c03a0f8192f99fac48cb9d8b1d5c7cd5c1895f
salim <s534027@nwmissouri.edu>
2021-05-11 23:23:06 -0500

    Added functional button to clear data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,11 @@
 
     <CountrySelect @get-country="getCountryData" :countries="countries" />
 
-    
+    <button @click="clearCountryData"
+    v-if="stats.Country"
+    class="bg-red-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-red-600">
+      Clear Country
+    </button>
   </main>
 
   <main class="flex flex-col align-center justify-center text-center" v-else>
@@ -52,6 +56,13 @@ export default {
     getCountryData(country){
       this.stats = country
       this.title = country.Country
+    },
+    async clearCountryData(){
+      this.loading = true
+      const data = await this.fetchCovidData()
+      this.title = 'Global'
+      this.stats = data.Global
+      this.loading = false
     }
   },
 
